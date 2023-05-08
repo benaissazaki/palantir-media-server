e14ef548dbc5d00c21d19fa7be9c60b14f108f55
lilxak <zakben45@gmail.com>
2023-05-08 19:35:51 +0100

    Add comment

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod routes;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let mut args = std::env::args().skip(1);
-
+    /* Get host and port as CLI args or use default values */
     let host = args.next().unwrap_or("127.0.0.1".to_string());
     let port = args
         .next()
