f67bfcf886382f577f99328e5b1e391707d2da9e
lilxak <zakben45@gmail.com>
2023-05-08 11:44:11 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
