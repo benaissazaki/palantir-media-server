d0be61f38cc80634e7dcc4b059db431bd9b336d0
lilxak <zakben45@gmail.com>
2023-05-09 11:00:20 +0100

    Create get_host_and_port function

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,24 @@ use actix_web::{App, HttpServer};
 
 mod routes;
 
+fn get_host_and_port() -> (String, u16) {
+    let mut args = std::env::args().skip(1);
+
+    let host = args.next().unwrap_or("127.0.0.1".to_string());
+    let port = match args.next() {
+        Some(p) => match p.parse::<u16>() {
+            Ok(port) => port,
+            Err(_) => 8080,
+        },
+        None => 8080,
+    };
+
+    (host, port)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut args = std::env::args().skip(1);
-    /* Get host and port as CLI args or use default values */
-    let host = args.next().unwrap_or("127.0.0.1".to_string());
-    let port = args
-        .next()
-        .unwrap_or("8080".to_string())
-        .parse::<u16>()
-        .unwrap();
-
+    let (host, port) = get_host_and_port();
     let server = HttpServer::new(|| App::new().service(routes::hello))
         .bind((host.clone(), port))?
         .run();
