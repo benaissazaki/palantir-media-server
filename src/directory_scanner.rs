4a373fd0cd111ebdb7005b64f540051a42a9e682
lilxak <zakben45@gmail.com>
2023-05-13 12:47:31 +0100

    Change scan_for_media_files' argument

--- src/directory_scanner.rs
+++ src/directory_scanner.rs
@@ -1,16 +1,16 @@
 use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 const MEDIA_FILES_EXTENSIONS: &'static [&'static str] = &["mp3", "mp4", "avi", "wav", "mkv"];
 
-pub fn scan_for_media_files(dir_path: &Path) -> Vec<PathBuf> {
+pub fn scan_for_media_files(dir_path: PathBuf) -> Vec<PathBuf> {
     let mut media_files = Vec::new();
 
     for entry in fs::read_dir(dir_path).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
         if path.is_dir() {
-            media_files.extend(scan_for_media_files(&path));
+            media_files.extend(scan_for_media_files(path));
         } else {
             if let Some(ext) = path.extension() {
                 if let Some(ext_str) = ext.to_str() {
