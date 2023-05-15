c467a9f73a6d5f8e64b50edf8250e52edce98a6a
lilxak <zakben45@gmail.com>
2023-05-15 16:13:14 +0100

    Setup eslint with airbnb-typescript

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'airbnb-typescript'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
