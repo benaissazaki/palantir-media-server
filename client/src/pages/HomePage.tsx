d55762565539b86bf299b5c556c218b90c075c35
lilxak <zakben45@gmail.com>
2023-05-16 12:48:18 +0100

    List media files in HomePage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,5 +1,35 @@
-export const HomePage = () => (
+import { useEffect, useState } from 'react';
+
+type MediaFilesResponse = {
+  length: number,
+  items: string[]
+};
+
+export const HomePage = () => {
+  const [mediaFiles, setMediaFiles] = useState<string[]>([]);
+
+  const fetchMediaFilesList = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/media`);
+      const data: MediaFilesResponse = await response.json();
+      setMediaFiles(data.items);
+    } catch (error) {
+      console.error('Error fetching media directories:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMediaFilesList();
+  }, []);
+
+  return (
     <>
-        <h1>Home</h1>
+      <h1>Home</h1>
+      <ul>
+        {mediaFiles.map(file => (
+          <li key={file}>{file}</li>
+        ))}
+      </ul>
     </>
-); 
+  );
+}; 
