98ab8aca0e173b34fa6cb3c5dd4d670e9811bc57
lilxak <zakben45@gmail.com>
2023-05-18 17:02:36 +0100

    Extract Settings into its own struct

--- src/app_settings/utils.rs
+++ src/app_settings/utils.rs
@@ -4,58 +4,46 @@ use std::{
     io::{Read, Seek, SeekFrom, Write},
 };
 
-const SETTING_FILE_NAME: &str = "app_settings.json";
+const SETTINGS_FILE_NAME: &str = "app_settings.json";
 
-pub fn get_setting(setting_name: String) -> Option<serde_json::Value> {
-    // Open the settings file
-    let mut file = match std::fs::File::open(SETTING_FILE_NAME) {
-        Ok(f) => f,
-        Err(_) => return None,
-    };
-
-    // Read the file contents into a string
-    let mut contents = String::new();
-    if let Err(_) = file.read_to_string(&mut contents) {
-        return None;
-    }
-
-    // Parse the JSON string into a Value
-    let settings: serde_json::Value = match serde_json::from_str(&contents) {
-        Ok(v) => v,
-        Err(_) => return None,
-    };
-
-    // Return the setting's value
-    settings.get(setting_name).cloned()
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct Settings {
+    pub media_directories: Option<Vec<String>>,
 }
 
-pub fn set_setting(
-    setting_name: String,
-    setting_value: serde_json::Value,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(SETTING_FILE_NAME)?;
+impl Settings {
+    pub fn load() -> Option<Self> {
+        // Open the settings file
+        let mut file = match std::fs::File::open(SETTINGS_FILE_NAME) {
+            Ok(f) => f,
+            Err(_) => return None,
+        };
 
-    // Read the file contents into a string
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+        // Read the file contents into a string
+        let mut contents = String::new();
+        if let Err(_) = file.read_to_string(&mut contents) {
+            return None;
+        }
 
-    let mut settings: serde_json::Value = if contents.is_empty() {
-        serde_json::Value::Null
-    } else {
-        serde_json::from_str(&contents)?
-    };
+        // Parse the JSON string into a Value
+        let settings: Settings = match serde_json::from_str(&contents) {
+            Ok(v) => return v,
+            Err(_) => return None,
+        };
+    }
 
-    settings[setting_name] = setting_value;
+    pub fn save(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(SETTINGS_FILE_NAME)?;
 
-    let new_contents = serde_json::to_string_pretty(&settings)?;
+        let json_string = serde_json::to_string_pretty(self)?;
 
-    file.set_len(new_contents.len() as u64)?;
-    file.seek(SeekFrom::Start(0))?;
-    file.write_all(new_contents.as_bytes())?;
+        file.set_len(json_string.len() as u64)?;
+        file.seek(SeekFrom::Start(0))?;
+        file.write_all(json_string.as_bytes())?;
 
-    Ok(())
+        Ok(())
+    }
 }
