7c80a4a2a6fc3e9defb47a3c8d2a1c6bac32a947
lilxak <zakben45@gmail.com>
2023-05-23 16:31:04 +0100

    Extract media files list into MediaList

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,36 +1,10 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
-type MediaFilesResponse = {
-  length: number,
-  items: string[]
-};
+import MediaList from '../components/MediaList';
 
 export const HomePage = () => {
-  const [mediaFiles, setMediaFiles] = useState<string[]>([]);
-
-  const fetchMediaFilesList = async () => {
-    try {
-      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/media`);
-      const data: MediaFilesResponse = await response.json();
-      setMediaFiles(data.items);
-    } catch (error) {
-      console.error('Error fetching media directories:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchMediaFilesList();
-  }, []);
-
   return (
     <>
       <h1>Home</h1>
-      <ul>
-        {mediaFiles.map(file => (
-          <li key={file}><Link to={`/media/${encodeURIComponent(file)}`}>{file}</Link></li>
-        ))}
-      </ul>
+      <MediaList />
     </>
   );
 }; 
