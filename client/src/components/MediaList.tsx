45b114eb9c35c6595e2c78ad14e4d7f6971c0e0d
lilxak <zakben45@gmail.com>
2023-05-28 16:33:56 +0100

    Represent media files as a tree

--- client/src/components/MediaList.tsx
+++ client/src/components/MediaList.tsx
@@ -6,14 +6,100 @@ type MediaFilesResponse = {
   items: string[]
 };
 
+type FileNode = {
+  name: string,
+  url: string,
+  children: FileNode[]
+};
+
+/**
+ * Recursively fills the tree with FileNode objects based on the given path segments.
+ * @param tree The current tree node to fill.
+ * @param pathSegments An array of path segments representing a file path.
+ */
+function buildTreeFromPaths(tree: FileNode, pathSegments: string[]) {
+  if (pathSegments.length === 0) {
+    return; // Terminate when all path segments have been processed.
+  }
+
+  const correspondingNode = tree.children.filter(c => c.name === pathSegments[0]);
+  if (correspondingNode.length === 0) {
+    // If there is no matching child node, create a new FileNode and add it as a child to the current node.
+    const newNode: FileNode = {
+      name: pathSegments[0],
+      url: `${tree.url}/${pathSegments[0]}`,
+      children: [],
+    };
+    tree.children.push(newNode);
+    buildTreeFromPaths(newNode, pathSegments.slice(1)); // Recursively fill the new node with remaining path segments.
+  } else {
+    buildTreeFromPaths(correspondingNode[0], pathSegments.slice(1));
+  }
+
+}
+
+/**
+ * Converts an array of file paths into a hierarchical tree structure represented by FileNode objects.
+ * @param paths An array of file paths.
+ * @returns The root FileNode representing the hierarchical tree structure.
+ */
+function constructFileTree(paths: string[]): FileNode {
+  const tree: FileNode = {
+    name: '$root',
+    url: '',
+    children: [],
+  };
+
+  paths.forEach(path => buildTreeFromPaths(tree, path.split(/[\\/]/))); // Build the tree by splitting each path into path segments and filling the tree.
+
+  return tree;
+}
+
+interface TreeProps {
+  tree: FileNode;
+}
+
+const TreeNode = ({ tree }: TreeProps) => {
+  // Don't display root node
+  if (tree.name === '$root') {
+    return (
+      <>
+        {tree.children.map((child, index) => (
+          <TreeNode key={index} tree={child} />
+        ))}
+      </>
+    );
+  }
+
+  // Don't display links for directories
+  if (tree.children.length !== 0) {
+    return (
+      <ul>
+        <li>{tree.name}</li>
+        {tree.children.map((child, index) => (
+          <TreeNode key={index} tree={child} />
+        ))}
+      </ul>
+    );
+  }
+  return (
+    <ul>
+      <li><Link to={`/media/${encodeURIComponent(tree.url)}`}>{tree.name}</Link></li>
+      {tree.children.map((child, index) => (
+        <TreeNode key={index} tree={child} />
+      ))}
+    </ul>
+  );
+};
+
 const MediaList = () => {
-  const [mediaFiles, setMediaFiles] = useState<string[]>([]);
+  const [mediaFilesTree, setMediaFilesTree] = useState<FileNode>({ name: '$root', url: '', children: [] });
 
   const fetchMediaFilesList = async () => {
     try {
       const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/media`);
       const data: MediaFilesResponse = await response.json();
-      setMediaFiles(data.items);
+      setMediaFilesTree(constructFileTree(data.items));
     } catch (error) {
       console.error('Error fetching media directories:', error);
     }
@@ -24,11 +110,7 @@ const MediaList = () => {
   }, []);
 
   return (
-    <ul>
-      {mediaFiles.map(file => (
-        <li key={file}><Link to={`/media/${encodeURIComponent(file)}`}>{file}</Link></li>
-      ))}
-    </ul>
+    <TreeNode tree={mediaFilesTree} />
   );
 };
 
