487abfd2c428cd396c29d78f7c6ebae065ea31d5
lilxak <zakben45@gmail.com>
2023-05-29 10:51:02 +0100

    Get path separator from media files

--- client/src/components/MediaList.tsx
+++ client/src/components/MediaList.tsx
@@ -17,23 +17,24 @@ type FileNode = {
  * @param tree The current tree node to fill.
  * @param pathSegments An array of path segments representing a file path.
  */
-function buildTreeFromPaths(tree: FileNode, pathSegments: string[]) {
+function buildTreeFromPaths(tree: FileNode, pathSegments: string[], isWindowsPathSeparator: boolean) {
   if (pathSegments.length === 0) {
     return; // Terminate when all path segments have been processed.
   }
 
   const correspondingNode = tree.children.filter(c => c.name === pathSegments[0]);
   if (correspondingNode.length === 0) {
+    const pathSeparator = isWindowsPathSeparator ? '\\' : '/';
     // If there is no matching child node, create a new FileNode and add it as a child to the current node.
     const newNode: FileNode = {
       name: pathSegments[0],
-      url: `${tree.url}/${pathSegments[0]}`,
+      url: `${tree.url && (tree.url + pathSeparator)}${pathSegments[0]}`,
       children: [],
     };
     tree.children.push(newNode);
-    buildTreeFromPaths(newNode, pathSegments.slice(1)); // Recursively fill the new node with remaining path segments.
+    buildTreeFromPaths(newNode, pathSegments.slice(1), isWindowsPathSeparator); // Recursively fill the new node with remaining path segments.
   } else {
-    buildTreeFromPaths(correspondingNode[0], pathSegments.slice(1));
+    buildTreeFromPaths(correspondingNode[0], pathSegments.slice(1), isWindowsPathSeparator);
   }
 
 }
@@ -50,7 +51,7 @@ function constructFileTree(paths: string[]): FileNode {
     children: [],
   };
 
-  paths.forEach(path => buildTreeFromPaths(tree, path.split(/[\\/]/))); // Build the tree by splitting each path into path segments and filling the tree.
+  paths.forEach(path => buildTreeFromPaths(tree, path.split(/[\\/]/), path.includes('\\'))); // Build the tree by splitting each path into path segments and filling the tree.
 
   return tree;
 }
