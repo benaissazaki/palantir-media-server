141c968468d73e3690ff7d40cb3cd1857ed70e96
lilxak <zakben45@gmail.com>
2023-06-08 12:25:44 +0100

    Slightly style Navbar

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
+import style from './Navbar.module.css';
 
-export const Navbar = () => (
-    <nav>
+const Navbar = () => (
+    <nav className={style.navbar}>
         <ul>
             <li><Link to="/">Home</Link></li>
         </ul>
     </nav>
 );
+
+export default Navbar;
