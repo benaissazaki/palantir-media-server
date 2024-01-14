3546747f7a33207ba756589a9882ab8791289537
Zaki Benaissa <zakben45@gmail.com>
2024-01-14 21:32:29 +0100

    build: create linux build script

---
+++ build-linux.sh
@@ -0,0 +1,6 @@
+cd client
+pnpm run build --emptyOutDir --outDir ../build
+cd ..
+cargo build --release
+cp target/release/palantir-media-server build/
+cp app_settings_example.json build/app_settings.json
